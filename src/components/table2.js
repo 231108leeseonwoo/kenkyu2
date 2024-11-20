2fbed8b1e7018a6dacab8417fff04a351091c23b
SEON WOO LEE <231108@st.yoshida-g.ac.jp>
2024-11-20 09:33:32 +0900

    a

--- src/components/table2.js
+++ src/components/table2.js
@@ -10,16 +10,15 @@ export default function Table2({ data }) {
     return <div>Loading...</div>; // データが不正な場合やまだ読み込まれていない場合
   }
 
-  // oddsはオブジェクトなので、キーを使ってアクセス
-  const events = Array.isArray(data.odds) ? data.odds : [data.odds]; // oddsが配列でない場合は配列に変換
-
+  // oddsはオブジェクトなので、その値を配列に変換する
+  const events = Object.values(data.odds); // oddsオブジェクトの値を配列に変換
 
   return (
     <div className="bg-gray-400 grid grid-cols-1 divide-y text-black">
       {events.map((odds) => (
         // odds.id がユニークであると仮定していますが、もし重複の可能性があれば
         // odds.id と市場名 (odds.marketName) を組み合わせて key を生成できます
-        <Link to={`/oddsDetail/${odds.id}`} key={`${odds.id}-${odds.marketName}`}>
+        <Link to={`/oddsDetail/${odds.id}`} key={odds.id}>
           <div className="bg-white py-2">
             <div className="flex justify-center">
               {/* marketName と marketId を表示 */}
@@ -37,15 +36,3 @@ export default function Table2({ data }) {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
